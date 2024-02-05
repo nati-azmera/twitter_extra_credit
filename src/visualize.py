c9e07b431adf7191fecc75e6b85e8ac38a3f7bc9
Nati Azmera <nazmera@cmc.edu>
2024-02-05 14:00:30 -0800

    updated

---
+++ src/visualize.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# command line args
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_path',required=True)
+parser.add_argument('--key',required=True)
+parser.add_argument('--percent',action='store_true')
+args = parser.parse_args()
+
+# imports
+import os
+import json
+from collections import Counter,defaultdict
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+# open the input path
+with open(args.input_path) as f:
+    counts = json.load(f)
+
+# normalize the counts by the total values
+if args.percent:
+    for k in counts[args.key]:
+        counts[args.key][k] /= counts['_all'][k]
+
+# print the count values
+items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
+top = items[:10]
+
+k = [x[0] for x in top]
+v = [x[1] for x in top]
+k = k[::-1]
+v = v[::-1]
+
+# plot
+plt.bar(range(len(k)), v)
+plt.xticks(range(len(k)), k)
+plt.ylabel('Total Tweets')
+
+# save bar graph as PNG file
+if args.input_path[-1] == 'g':
+    plt.savefig(args.key[1:] + '_lang.png')
+else:
+    plt.savefig(args.key[1:] + '_country.png')
